7c0c2ffb1f95c03507f263d2e82ee027cabf8193
siriwat <siriwat_da@cmu.ac.th>
2026-01-04 09:51:23 +0700

    Commit Task 10

--- src/for.ts
+++ src/for.ts
@@ -1,21 +1,9 @@
-// let i = 0;
-// for (i = 0; i < 10; i++) {
-//     console.log(i);
-// }
-//
-// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
-// for (let i = 0; i < color.length; i++) {
-//     console.log(color[i])
-// }
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for (const c of color) {
+    console.log(c);
+}
 
 
-console.log("Name Check Challenge");
-
-let names: string[] = ["Robe", "Jonathan", "Ann", "Benjamin", "May"];
-for (let i = 0; i < names.length; i++) {
-    if (names[i].length > 6) {
-        console.log("สวัสดีคุณ " + names[i]);
-    } else {
-        console.log("Hello " + names[i]);
-    }
+for (const index in color) {
+    console.log(`Index: ${index}, Value: ${color[index]}`);
 }
