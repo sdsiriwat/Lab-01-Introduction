99cca1226d6ed2490189d7213a5108e67b1b1f35
siriwat <siriwat_da@cmu.ac.th>
2026-01-04 09:36:39 +0700

    Add grade calculation

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
-// let x:number = 11
+// let x =11
 // if (x > 10) {
 //     console.log('x is more than 10')
+// } else if (x < 5){
+//     console.log('x is less than 5')
+// }
+// else {
+//     console.log('x is between 5 and 10')
 // }
 
 
-// let x:number = 9
-// if (x > 10) {
-//     console.log('x is more than 10')
-// }else {
-//     console.log('x is less than 10')
+// let x: number = 4
+// let y: number = 11
+// if (x > 5 && y > 12) {
+//     console.log("ค่ามากจัง");
+// } else {
+//
+//     console.log((x * y) + (x / y));
 // }
 
-let x: number = 4
-let y: number = 11
 
-if (x > 5 && y > 12) {
-    console.log("ค่ามากจัง");
+let score: number = 85;
+
+// เริ่มเช็คเงื่อนไข
+if (score > 80) {
+    console.log("Grade A");
+} else if (score > 60) {
+    console.log("Grade B");
+} else if (score > 50) {
+    console.log("Grade C");
 } else {
-    console.log((x * y) + (x / y));
+    console.log("Grade F");
 }
